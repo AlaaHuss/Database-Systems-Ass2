bf68246897b687427d3d12e282efbc2077392168
AlaaHuss <s3612653@student.rmit.edu.au>
2019-05-16 21:01:33 +0300

    dbload main

--- src/dbload.java
+++ src/dbload.java
@@ -1,13 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Lina
- */
 public class dbload {
-    
+    public static void main(String[] args) throws Exception {
+        if(args.length > 0){
+            if (args.length!=3){
+                System.out.println("You should input parameters like this. -p <HeapSize> <SourceFile>");
+                return;
+            }else if(args[0].equals("-p")) {
+                int pageSize = Integer.parseInt(args[1]);;
+                String fileName = args[2];
+            }else{
+                System.out.println("You should input correct parameter.");
+                return;
+            }
+        }else{
+            System.out.println("You should input parameters like this. -p <HeapSize> <SourceFile>");
+            return;
+        }
+    }
 }

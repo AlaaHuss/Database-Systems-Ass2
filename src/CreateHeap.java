4395ff64c02c16152e08c5b9a531af87c132c069
AlaaHuss <s3612653@student.rmit.edu.au>
2019-05-16 21:46:44 +0300

    Create heap

--- src/CreateHeap.java
+++ src/CreateHeap.java
@@ -1,7 +1,12 @@
 
 class CreateHeap {
+    int pageSize;
+    int recordSize = 197;
 
-    CreateHeap(int pageSize) {
+    int[] sizes = new int[] {4,22,22,4,6,40,15,4,23,22,26,4,5};
+    
+    public CreateHeap(int pageSize) {
+        this.pageSize = pageSize;
     }
 
     void save(String fileName) {

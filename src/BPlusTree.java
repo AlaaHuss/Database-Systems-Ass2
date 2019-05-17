3932f6896720ec1231e8ca96693d149d50d7ffc7
AlaaHuss <s3612653@student.rmit.edu.au>
2019-05-17 19:03:14 +0300

    b+ tree

--- src/BPlusTree.java
+++ src/BPlusTree.java
@@ -1,13 +1,39 @@
+public class BPlusTree<K extends Comparable<? super K>, V> {
+    
+    /**
+    * The branching factor for the B+ tree, that measures the capacity of nodes
+    * (i.e., the number of children nodes) for internal nodes in the tree.
+    */
+   private int branchingFactor;
 
-class BPlusTree<T0, T1> {
+   /**
+    * The root node of the B+ tree.
+    */
+   private Node root;
 
-    BPlusTree(int i) {
+    public BPlusTree(int branchingFactor) {
+        if (branchingFactor <= 2)
+            throw new IllegalArgumentException("Illegal branching factor: " + branchingFactor);
+        this.branchingFactor = branchingFactor;
+        root = new LeafNode();
     }
 
     void insert(String key, String valueOf) {
     }
 
     void writeIndex(int pageSize) {
+    }
+
+    private static class Node {
+
+        public Node() {
+        }
+    }
+
+    private static class LeafNode extends Node {
+
+        public LeafNode() {
+        }
     }
     
 }

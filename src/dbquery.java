6e27f4805441009a73164d27e84fd8a7474b7ab3
AlaaHuss <s3612653@student.rmit.edu.au>
2019-05-18 21:26:40 +0300

    dbquery main

--- src/dbquery.java
+++ src/dbquery.java
@@ -1,4 +1,16 @@
 
 public class dbquery {
-    
+    public static void main(String[] args) throws Exception {
+            if (args.length!=2){  //check parameter
+                System.err.println("Please correct parameter");
+                return;
+            }
+
+            String txt = args[0];
+            int pageSize = Integer.parseInt(args[1]);
+
+            CreateHeap h = new CreateHeap(pageSize);
+
+	}
+
 }
